ecedf15b313e4bb110b2297d4f3f4585564e959b
melanie-attard <melattard15@gmail.com>
2023-01-05 10:20:45 +0100

    Added aggregates for travel history

--- MyApp/BusSummaryPage.xaml.cs
+++ MyApp/BusSummaryPage.xaml.cs
@@ -1,3 +1,5 @@
+using MyApp.Models;
+
 namespace MyApp;
 
 public partial class BusSummaryPage : ContentPage
@@ -5,7 +7,18 @@ public partial class BusSummaryPage : ContentPage
 	public BusSummaryPage()
 	{
 		InitializeComponent();
-		// grouping by route id, so we do not display repeated data.
-		busSummary.ItemsSource = App.AppRepo.Manager.BusesUsed.GroupBy(route => route.Id).Select(bus => bus.FirstOrDefault());
+
+		List<Route> busesUsed = App.AppRepo.Manager.BusesUsed;
+        // grouping by route id, to avoid displaying repeated data
+        busSummary.ItemsSource = busesUsed.GroupBy(route => route.Id).Select(bus => bus.FirstOrDefault());
+
+		// aggregate information
+		int total = busesUsed.Count;
+		info.Text = "Total Buses Used Today: " + total;
+
+		foreach(var group in busesUsed.GroupBy(route => route.Id))
+		{
+			info.Text += "\nYou used Route " + group.Key + ", " + group.Count() + " time(s).";
+		}
 	}
 }
